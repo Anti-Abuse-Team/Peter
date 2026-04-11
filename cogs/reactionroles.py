6b69bbc5f8b25c96cdea7abd8299ce188e45c5dd
optionest <optionest12@gmail.com>
2026-04-11 12:38:46 -0500

    reaction role fix

--- cogs/reactionroles.py
+++ cogs/reactionroles.py
@@ -15,13 +15,17 @@ class ReactionRoles(commands.Cog):
     @reactions.command(name="send", description="Sends reaction roles")
     async def register(self, ctx: commands.Context):    
         if any(r.id in admin for r in ctx.author.roles) or ctx.author.guild_permissions.administrator:
-            await ctx.send(view=view)
-            view = LeaveReactionRoles()
-            await ctx.send(view=view)
-            view = ColorReactionRoles()
-            await ctx.send(view=view)
+            try:
+                await ctx.send(view=view)
+                view = LeaveReactionRoles()
+                await ctx.send(view=view)
+                view = ColorReactionRoles()
+                await ctx.send(view=view)
+            except Exception as E:
+                await ctx.send(E)
         else:
             await ctx.send('no perms.')
+    
     
 async def setup(bot):
     await bot.add_cog(ReactionRoles(bot))
