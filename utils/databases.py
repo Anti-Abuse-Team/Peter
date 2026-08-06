5472f46de3a141cacec4e3bf69458211491fe0ea
ZeroTrojZX <robertctetherclaw@gmail.com>
2026-08-06 13:02:25 -0400

    Update databases.py
    Signed-off-by: ZeroTrojZX <robertctetherclaw@gmail.com>

--- utils/databases.py
+++ utils/databases.py
@@ -12,7 +12,6 @@ mongodb = MongoClient(os.getenv("MONGODB_URL"))
 db = mongodb["AAT"]
 roles_db = db["roles"]
 notes_db = db["notes"]
-warns_db = db["warns"]
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
     os.getenv("MONGODB_URL"),
